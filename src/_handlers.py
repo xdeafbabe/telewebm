0747aa9176c9f272ebc7add71b0b86535c7208e5
Euromancer <euromancer@icloud.com>
2022-02-18 16:04:51 +0300

    ...

--- src/_handlers.py
+++ src/_handlers.py
@@ -1,8 +1,73 @@
+import asyncio
+import asyncio.subprocess
+import io
+
+import aiofiles
 import aiogram.types
+import httpx
+import pydantic
 
 import _bot
 
 
+class URL(pydantic.BaseModel):
+    address: pydantic.HttpUrl
+
+
 @_bot.dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: aiogram.types.Message) -> None:
+async def send_welcome(message: aiogram.types.Message):
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@_bot.dp.message_handler()
+async def convert(message: aiogram.types.Message) -> None:
+    status_message = await message.answer('Working...')
+
+    if not message.text:
+        await status_message.edit_text('Not a link. Aborted.')
+        return
+
+    try:
+        url = URL(address=message.text)
+    except pydantic.ValidationError:
+        await status_message.edit_text('Not a link. Aborted.')
+        return
+
+    async with httpx.AsyncClient(http2=True) as client:
+        resp = await client.head(url.address)
+
+        if resp.status_code != 200 or resp.headers.get('content-type') != 'video/webm':
+            await status_message.edit_text('Link target is not a WebM file. Aborted.')
+            return
+
+        async with aiofiles.tempfile.TemporaryDirectory() as tmpdir:
+            input_file_path = f'{tmpdir}/in.webm'
+            output_file_path = f'{tmpdir}/out.mp4'
+
+            await status_message.edit_text('Downloading...')
+
+            async with aiofiles.open(input_file_path, 'wb') as input_file:
+                async with client.stream('GET', url.address) as r:
+                    async for chunk in r.aiter_bytes():
+                        await input_file.write(chunk)
+
+            await status_message.edit_text('Converting...')
+
+            process = await asyncio.create_subprocess_exec(
+                'ffmpeg', '-i', input_file_path, output_file_path,
+                stderr=asyncio.subprocess.DEVNULL)
+
+            try:
+                await asyncio.wait_for(process.wait(), timeout=5)
+            except asyncio.TimeoutError:
+                await status_message.edit_text('Conversion took too long. Aborted.')
+                return
+
+            if process.returncode != 0:
+                await status_message.edit_text('Failed to convert. Aborted.')
+                return
+
+            async with aiofiles.open(output_file_path, 'rb') as output_file:
+                await _bot.bot.send_video(message.chat.id, output_file)
+
+    await status_message.delete()
--- src/main.py
+++ src/main.py
@@ -5,11 +5,10 @@ import aiogram
 import dotenv
 
 import _bot
+import _handlers  # noqa: F401
 
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
     logging.basicConfig(level=os.getenv('LOGGING_LEVEL', logging.DEBUG))
     aiogram.executor.start_polling(_bot.dp, skip_updates=True)
-
-    print('Hello from WebM2MP4Bot!')
