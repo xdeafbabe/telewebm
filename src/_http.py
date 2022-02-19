d5b5d63a23f3dcdd32822474b950cd3dcfef7c63
Euromancer <euromancer@icloud.com>
2022-02-19 20:30:24 +0300

    ...

--- src/_http.py
+++ src/_http.py
@@ -23,6 +23,6 @@ async def check_headers(url: str) -> _utils.StatusEnum:
 async def download_file(url: str, output_file_path: str) -> None:
     async with aiofiles.open(output_file_path, 'wb') as output_file:
         async with httpx.AsyncClient(http2=True) as client:
-            async with client.stream('GET', url) as r:
-                async for chunk in r.aiter_bytes():
+            async with client.stream('GET', url) as resp:
+                async for chunk in resp.aiter_bytes():
                     await output_file.write(chunk)
